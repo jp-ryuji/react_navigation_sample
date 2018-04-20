ea24ab23814717ee34a521250d87130ea62b4fc3
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-20 13:17:51 +0900

    Export HomeScreen

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './src/screens/HomeScreen';
 
 export default class App extends React.Component {
   render() {
