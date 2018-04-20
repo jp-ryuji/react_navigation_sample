818d8ac00c89057767f59b8e34d2354c8ba6cafc
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-20 13:44:02 +0900

    Go back from DetailsScreen to HomeScreen

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 
 export default class DetailsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
       </View>
     );
   }
