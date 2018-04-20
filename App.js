cb165f510d0c76fba90040612ab0c12fe64fb7bf
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-20 13:21:06 +0900

    Add DetailsScreen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
+import DetailsScreen from './src/screens/DetailsScreen';
 
 export default class App extends React.Component {
   render() {
@@ -9,8 +10,16 @@ export default class App extends React.Component {
   }
 }
 
-const RootStack = StackNavigator({
-  Home: {
-    screen: HomeScreen,
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
   },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
