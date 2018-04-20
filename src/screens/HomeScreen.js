ebcec028951e77f2a48f7e544e070024231d6d11
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-21 08:37:06 +0900

    Header interaction with its screen component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,15 +13,27 @@ class LogoTitle extends React.Component {
 }
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    headerTitle: <LogoTitle />,
-    headerRight: (
-      <Button
-        onPress={() => alert('This is a button')}
-        title="info"
-        color="#fff"
-      />
-    )
+  static navigationOptions = ({ navigation }) => {
+    const params = navigation.state.params || {};
+
+    return {
+      headerTitle: <LogoTitle />,
+      headerRight: (
+        <Button onPress={params.increaseCount} title="+1" color="#fff" />
+      ),
+    };
+  };
+
+  componentWillMount() {
+    this.props.navigation.setParams({ increaseCount: this._increaseCount });
+  }
+
+  state = {
+    count: 0,
+  };
+
+  _increaseCount = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
