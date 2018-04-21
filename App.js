d1e865504285b71249c1e91012b3d8afed04a027
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-21 09:58:57 +0900

    Customizing the appearance

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
 
 class HomeScreen extends React.Component {
   render() {
@@ -22,7 +23,36 @@ class SettingsScreen extends React.Component {
   }
 }
 
-export default TabNavigator({
-  Home: { screen: HomeScreen },
-  Settings: { screen: SettingsScreen },
-});
+export default TabNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Settings: { screen: SettingsScreen },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarComponent: TabBarBottom,
+    // NOTE: The default behavior of TabNavigator is to show a tab bar on the top of the screen
+    //   on Android and on the bottom on iOS, but here we force it to be on the bottom on both platforms.
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
